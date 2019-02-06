b2a84651616bedc716448b9c88e41324a2428024
Elle Hallal <daniellehallal@gmail.com>
2019-02-06 14:23:41 +0000

    Tests passing: renders ApprovalCard based on number of items in this.state.comments, renders CommentDetail instances based number of ApprovalCard instances

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
 import ApprovalCard from "./components/ApprovalCard";
+import CommentDetail from "./components/CommentDetail";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
@@ -12,6 +13,16 @@ const findByTestAttr = (wrapper, val) => {
 
 test("renders without crashing", () => {
   shallow(<App />);
+});
+
+test("renders ApprovalCard based on number of items in this.state.comments", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find("ApprovalCard").length).toBe(4);
+});
+
+test("renders CommentDetail instances based number of ApprovalCard instances", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find("CommentDetail").length).toBe(4);
 });
 
 test("displays number of comments", () => {
