f21fe6d2448cb8c9fe049702ec3e60e45fec6d1c
Elle Hallal <daniellehallal@gmail.com>
2019-02-06 11:40:12 +0000

    Change CommentDetail from stateful component to stateless

--- src/components/CommentDetail.js
+++ src/components/CommentDetail.js
@@ -1,54 +1,45 @@
 import React from "react";
 import faker from "faker";
 
-export default class CommentDetail extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+const CommentDetail = props => {
+  function displayAvatar() {
+    return props.avatar || faker.image.avatar();
   }
 
-  displayAvatar = () => {
-    return this.props.avatar || faker.image.avatar();
-  };
+  function displayAuthor() {
+    return props.author || faker.name.firstName();
+  }
 
-  displayAuthor = () => {
-    return this.props.author || faker.name.firstName();
-  };
+  function displayDate() {
+    return props.date || faker.date.past().toString();
+  }
 
-  displayDate = () => {
-    return this.props.date || faker.date.past().toString();
-  };
+  function displayCommentText() {
+    return props.text || faker.lorem.sentence();
+  }
 
-  displayCommentText = () => {
-    return this.props.text || faker.lorem.sentence();
-  };
-
-  render() {
-    return (
-      <div className="ui container comments">
-        <div className="comment">
-          <a href="/" className="avatar">
-            <img
-              alt="avatar"
-              data-test="avatar-image"
-              src={this.displayAvatar()}
-            />
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" data-test="avatar-image" src={displayAvatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author" data-test="author">
+            {displayAuthor()}
           </a>
-          <div className="content">
-            <a href="/" className="author" data-test="author">
-              {this.displayAuthor()}
-            </a>
-            <div className="metadata">
-              <span className="date" data-test="date">
-                {this.displayDate()}
-              </span>
-            </div>
-            <div className="text" data-test="text">
-              {this.displayCommentText()}
-            </div>
+          <div className="metadata">
+            <span className="date" data-test="date">
+              {displayDate()}
+            </span>
+          </div>
+          <div className="text" data-test="text">
+            {displayCommentText()}
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default CommentDetail;
